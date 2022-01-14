2c0bbaa0390fbbce8030e08cdc1b66a80fe021fa
Nicholas Berlette <nick@berlette.com>
2022-01-14 11:05:25 -0800

    v0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,55 @@
-import {
-  writable as _internal, 
-  get as _get_store,
-  Writable
-} from 'svelte/store';
+import {writable as internal, get, Writable} from 'svelte/store'
 
-type Updater<T> = (value: T) => T;
+declare type Updater<T> = (value: T) => T;
+declare type StoreDict<T> = { [key: string]: Writable<T> }
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-type StoreDict<T = any> = { [key: string]: Writable<T> };
-const stores: StoreDict = {};
-const browser: boolean  = (typeof(window) != undefined);
-const storage: any = localStorage || (browser ? (window.localStorage || new Map()) : new Map());
+const stores: StoreDict<any> = {}
 
-function writable<T>(key: string, initialValue: T): Writable<T> {
+export function writable<T>(key: string, initialValue: T): Writable<T> {
+  const browser = typeof window != 'undefined' && ('localStorage' in window)
 
-  function _update_store (key: string, value: T): void {
-    storage[key] = JSON.stringify(value)
+  function updateStorage(key: string, value: T) {
+    if (browser) localStorage.setItem(key, JSON.stringify(value))
   }
 
   if (!stores[key]) {
-    const store = _internal(initialValue, (set) => {
-      const json = storage.has(key) ? storage[key] : null;
-      if (json) set(<T> JSON.parse(json));
+    const store = internal(initialValue, (set) => {
+      const json = browser ? localStorage.getItem(key) : null
+      if (json) set(<T> JSON.parse(json))
 
       if (browser) {
         const handleStorage = (event: StorageEvent) => {
-          if (event.key === key) set(event.newValue ? JSON.parse(event.newValue) : null)
+          if (event.key === key)
+            set(event.newValue ? JSON.parse(event.newValue) : null)
         }
 
         window.addEventListener("storage", handleStorage)
-        return () => window.removeEventListener("storage", handleStorage)
+	return () => {
+	  window.removeEventListener("storage", handleStorage)
+      	}
       }
-    });
+    })
+
+    const { subscribe, set } = store;
 
     stores[key] = {
-      set: (value: T) => {
-        _update_store(key, value)
-        store.set(value)
+      set(value: T) {
+        updateStorage(key, value);
+        set(value);
       },
-      update: (updater: Updater<T>) => {
-        const value = updater(_get_store(store))
-        _update_store(key, value)
-        store.set(value)
+
+      update(updater: Updater<T>) {
+        const value = updater(get(store));
+
+        updateStorage(key, value);
+        set(value);
       },
-      subscribe: store.subscribe
+
+      subscribe
     }
   }
 
   return stores[key]
 }
 
-export {
-  writable,
-  stores,
-  browser,
-  storage,
-}
-export type {
-  Writable,
-  Updater,
-  StoreDict,
-}
